2b40f937667cdea3cd9884f19e77695cdfd29cef
Oleg Mokhov <olegmokhovural@gmail.com>
2026-05-16 13:59:14 +0300

    Выполнена проверка PEP8

--- src/processing.py
+++ src/processing.py
@@ -1,48 +1,50 @@
-def filter_by_state(list_dict: list, state: str = 'EXECUTED') -> list:
-    'Функция, которая фильтрует список словарей по значению ключа state'
+def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+    "Функция, которая фильтрует список словарей по значению ключа state"
     filtered_list = []
     for item in list_dict:
-        if item.get('state') == state:
+        if item.get("state") == state:
             filtered_list.append(item)
     return filtered_list
 
+
 operations = [
-    {'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+    {"id": 414288290, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
-'''Операции executed'''
+"""Операции executed"""
 executed_operations = filter_by_state(operations)
 print(executed_operations)
 
-'''Операции canceled'''
-canceled_operations = filter_by_state(operations, 'CANCELED')
+"""Операции canceled"""
+canceled_operations = filter_by_state(operations, "CANCELED")
 print(canceled_operations)
 
 
 def sort_by_date(list_dict: list, descending: bool = True) -> list:
-    '''Функция сортировки по дате'''
+    """Функция сортировки по дате"""
     sorted_list = list_dict.copy()
 
-    sorted_list.sort(key=lambda item: item.get('date', ''), reverse=descending)
+    sorted_list.sort(key=lambda item: item.get("date", ""), reverse=descending)
 
     return sorted_list
 
+
 operations = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
 ]
 
-'''Сортировка по убыванию сначала самые новые'''
+"""Сортировка по убыванию сначала самые новые"""
 sorted_desc = sort_by_date(operations)
 print("Сортировка по убыванию (новые → старые):")
 print(sorted_desc)
 
-'''Сортировка по возрастанию'''
+"""Сортировка по возрастанию"""
 sorted_asc = sort_by_date(operations, descending=False)
 print("\nСортировка по возрастанию (старые → новые):")
 print(sorted_asc)
