48a93cc4dfc713f83a784ab178b66a3e5753b1e6
Oleg Mokhov <olegmokhovural@gmail.com>
2026-05-23 19:29:38 +0300

    Добавлены тесты по дате

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -19,3 +19,28 @@ def test_filter_by_state_executed(state, expected_data, operations_data):
     assert result == expected
 
 
+@pytest.mark.parametrize('descending, expected_order', [
+    (
+        True,
+        [
+            {"id": 41428829, "date": "2019-07-03T18:35:29.512364"},
+            {"id": 615064591, "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+            {"id": 939719570, "date": "2018-06-30T02:08:58.425572"},
+        ]
+    ),
+    (
+        False,
+        [
+            {"id": 939719570, "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "date": "2018-10-14T08:21:33.419441"},
+            {"id": 41428829, "date": "2019-07-03T18:35:29.512364"},
+        ]
+    ),
+])
+def test_sort_by_date(sample_operations, descending, expected_order):
+    result = sort_by_date(sample_operations, descending=descending)
+    for i, expected in enumerate(expected_order):
+        assert result[i]["id"] == expected["id"]
+        assert result[i]["date"] == expected["date"]
