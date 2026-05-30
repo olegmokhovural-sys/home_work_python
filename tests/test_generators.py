3a98c63abc9fb05d05e210d52610fd68f8ee9a1f
Oleg Mokhov <olegmokhovural@gmail.com>
2026-05-30 13:22:58 +0300

    Написан тест на первую функцию

---
+++ tests/test_generators.py
@@ -0,0 +1,81 @@
+import pytest
+from typing import Generator
+from src.generators import filter_by_currency
+
+
+@pytest.mark.parametrize("currency_code, expected_ids", [
+
+    (
+            "USD",
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                },
+                {
+                    "id": 142264268,
+                    "state": "EXECUTED",
+                    "date": "2019-04-04T23:20:05.206878",
+                    "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод со счета на счет",
+                    "from": "Счет 19708645243227258542",
+                    "to": "Счет 75651667383060284188",
+                },
+                {
+                    "id": 895315941,
+                    "state": "EXECUTED",
+                    "date": "2018-08-19T04:27:37.904916",
+                    "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод с карты на карту",
+                    "from": "Visa Classic 6831982476737658",
+                    "to": "Visa Platinum 8990922113665229",
+                },
+            ]
+    ),
+
+    (
+            "RUB",
+            [
+                {
+                    "id": 873106923,
+                    "state": "EXECUTED",
+                    "date": "2019-03-23T01:09:46.296404",
+                    "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+                    "description": "Перевод со счета на счет",
+                    "from": "Счет 44812258784861134719",
+                    "to": "Счет 74489636417521191160",
+                },
+                {
+                    "id": 594226727,
+                    "state": "CANCELED",
+                    "date": "2018-09-12T21:27:25.241689",
+                    "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+                    "description": "Перевод организации",
+                    "from": "Visa Platinum 1246377376343588",
+                    "to": "Счет 14211924144426031657",
+                },
+            ]
+    ),
+
+    ("EUR", []),
+
+    ("", []),
+])
+def test_filter_by_currency(sample_transactions, currency_code, expected_ids):
+
+    result_gen = filter_by_currency(sample_transactions, currency_code)
+
+
+    result_list = list(result_gen)
+
+    # Проверяем длину
+    assert len(result_list) == len(expected_ids)
+
+    # Проверяем содержимое (сравниваем полные словари)
+    for i, expected_item in enumerate(expected_ids):
+        assert result_list[i] == expected_item
